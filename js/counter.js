ce31e69336acc07529516b5df9de3832f32f8065
Nora-Roselee <125979733+Nora-Roselee@users.noreply.github.com>
2023-08-09 12:26:41 +0200

    creazione pagina CSS, aggiunta class con JS

--- js/counter.js
+++ js/counter.js
@@ -4,16 +4,19 @@ let boxCounter = document.getElementById("boxCounter");
 // Creazione del bottone "minus"
 let minus = document.createElement("button");
 minus.innerText = "-";
+minus.className = "minor";
 boxCounter.appendChild(minus);
 
 // Creazione dello span "counter"
 let counter = document.createElement("span");
 counter.innerText = 0;
+counter.className = "number";
 boxCounter.appendChild(counter);
 
 // Creazione del bottone "plus"
 let plus = document.createElement("button");
 plus.innerText = "+";
+plus.className = "major";
 boxCounter.appendChild(plus);
 
 let reset = document.getElementById("reset");
@@ -21,6 +24,7 @@ let reset = document.getElementById("reset");
 // Creazione del bottone "reset"
 let resbtn = document.createElement("button");
 resbtn.innerText = "reset";
+resbtn.className = "tabulaRasa";
 reset.appendChild(resbtn);
 
 // Funzioni
